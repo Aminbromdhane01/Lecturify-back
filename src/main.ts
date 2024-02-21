bf83aa8e42b3bcd3a2c3a433e3eb820487e6f076
AminBromdhane01 <mohamedaminebenromdhane.com>
2024-02-21 17:45:37 +0100

    Initialize the project

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+require('dotenv').config();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = app.get(ConfigService)
+  const config_swagger = new DocumentBuilder()
+    .setTitle('Lecturify')
+    .setDescription('Lecturify API description')
+    .setVersion('1.0')
+    .addTag('LCT')
+    .build();
+  const document = SwaggerModule.createDocument(app, config_swagger);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(config.get('port'));
+}
+bootstrap();
