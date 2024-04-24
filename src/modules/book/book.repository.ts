b7e8bda6175bc82a61de914ad89e218be0efed33
AminBromdhane01 <mohamedaminebenromdhane.com>
2024-04-24 12:23:08 +0100

    LEC-15 : Implement CRUD operations in BookRepository

---
+++ src/modules/book/book.repository.ts
@@ -0,0 +1,49 @@
+import { AbstractGenericRepository } from '@app/comon/baserepository';
+import { Inject, Injectable } from '@nestjs/common';
+import { DataSource } from 'typeorm';
+
+import { Book } from './book.entity';
+import type { CreateBookDto } from './dto/create.book.dto';
+import type { GetBooksByPaginationDto } from './dto/get-book-by-pagination.dto';
+import type { UpdateBookDto } from './dto/update-book.dto';
+import type { IBookRepository } from './interfaces/book.repository.interface';
+
+@Injectable()
+export class BookReposotiroy
+  extends AbstractGenericRepository<Book>
+  implements IBookRepository
+{
+  constructor(
+    @Inject(DataSource) private readonly datasource: DataSource,
+  ) {
+    super(datasource, Book);
+  }
+
+  createBook(book: CreateBookDto): Promise<Book> {
+    return this.createItem('Book', book);
+  }
+
+  getAll({
+    itemPerPage,
+    page,
+    keyword,
+  }: GetBooksByPaginationDto): Promise<{ data: Book[]; count: number }> {
+    return this.findAll('Book', {
+      itemsPerPage: itemPerPage,
+      page,
+      keyword,
+    });
+  }
+
+  getBookbyId(id: number): Promise<Book | null> {
+    return this.findOnebyId('Book', id);
+  }
+
+  deleteBook(id: number): Promise<number> {
+    return this.deleteItem('Book', id);
+  }
+
+  updateBook(id: number, book: UpdateBookDto): Promise<Book | null> {
+    return this.updateItem('Book', id, book);
+  }
+}
