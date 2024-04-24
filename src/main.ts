8bce93371bfb7c91e1e5ecc03b7afe5ac09540b1
AminBromdhane01 <mohamedaminebenromdhane.com>
2024-04-24 12:22:28 +0100

    LEC-15 : Implement global Class Serializer Interceptor and HTTP Filter

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,25 @@
 import { AppModule } from '@app/app.module';
-import { HttpExceptionFilter } from '@app/comon/http-exception.filter';
+//import { HttpExceptionFilter } from '@app/comon/http-exception.filter';
 import { setupSwagger } from '@app/comon/swagger';
 import { envConstants } from '@app/config/constants';
+import {
+  ClassSerializerInterceptor,
+  ValidationPipe,
+} from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
+
+import { HttpExceptionFilter } from './comon/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const reflector = app.get(Reflector);
   const configService = app.get(ConfigService);
-  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalFilters(new HttpExceptionFilter(configService));
   app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(reflector));
 
   setupSwagger(app);
   app.enableCors();
@@ -20,4 +29,4 @@ async function bootstrap() {
   );
 }
 
-bootstrap();
+void bootstrap();
